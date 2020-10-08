fbe28e4a023b3de231c798f0316260fb083392df
CDHelder <72396857+CDHelder@users.noreply.github.com>
2020-10-08 17:28:42 +0200

    08-10-2020

--- Rekenmachine/Form1.Designer.cs
+++ Rekenmachine/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Rekenmachine
 {
-    partial class Form1
+    partial class Rekenmachine
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,243 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Rekenmachine));
+            this.Knop_nul = new System.Windows.Forms.Button();
+            this.Knop_haakje = new System.Windows.Forms.Button();
+            this.Knop_is = new System.Windows.Forms.Button();
+            this.Knop_1 = new System.Windows.Forms.Button();
+            this.Knop_2 = new System.Windows.Forms.Button();
+            this.Knop_drie = new System.Windows.Forms.Button();
+            this.Knop_plus = new System.Windows.Forms.Button();
+            this.Knop_min = new System.Windows.Forms.Button();
+            this.Knop_zes = new System.Windows.Forms.Button();
+            this.Knop_vijf = new System.Windows.Forms.Button();
+            this.Knop_vier = new System.Windows.Forms.Button();
+            this.Knop_keer = new System.Windows.Forms.Button();
+            this.Knop_negen = new System.Windows.Forms.Button();
+            this.Knop_acht = new System.Windows.Forms.Button();
+            this.Knop_zeven = new System.Windows.Forms.Button();
+            this.Knop_delen = new System.Windows.Forms.Button();
+            this.Knop_backspace = new System.Windows.Forms.Button();
+            this.Knop_C = new System.Windows.Forms.Button();
+            this.Knop_CE = new System.Windows.Forms.Button();
+            this.Knop_percentage = new System.Windows.Forms.Button();
+            this.Knop_euro = new System.Windows.Forms.Button();
+            this.Textbox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Knop_nul
+            // 
+            resources.ApplyResources(this.Knop_nul, "Knop_nul");
+            this.Knop_nul.Name = "Knop_nul";
+            this.Knop_nul.UseVisualStyleBackColor = true;
+            this.Knop_nul.Click += new System.EventHandler(this.Knop_click);
+            // 
+            // Knop_haakje
+            // 
+            resources.ApplyResources(this.Knop_haakje, "Knop_haakje");
+            this.Knop_haakje.Name = "Knop_haakje";
+            this.Knop_haakje.UseVisualStyleBackColor = true;
+            this.Knop_haakje.Click += new System.EventHandler(this.Knop_click);
+            // 
+            // Knop_is
+            // 
+            resources.ApplyResources(this.Knop_is, "Knop_is");
+            this.Knop_is.Name = "Knop_is";
+            this.Knop_is.UseVisualStyleBackColor = true;
+            // 
+            // Knop_1
+            // 
+            resources.ApplyResources(this.Knop_1, "Knop_1");
+            this.Knop_1.Name = "Knop_1";
+            this.Knop_1.UseVisualStyleBackColor = true;
+            this.Knop_1.Click += new System.EventHandler(this.Knop_click);
+            // 
+            // Knop_2
+            // 
+            resources.ApplyResources(this.Knop_2, "Knop_2");
+            this.Knop_2.Name = "Knop_2";
+            this.Knop_2.UseVisualStyleBackColor = true;
+            this.Knop_2.Click += new System.EventHandler(this.Knop_click);
+            // 
+            // Knop_drie
+            // 
+            resources.ApplyResources(this.Knop_drie, "Knop_drie");
+            this.Knop_drie.Name = "Knop_drie";
+            this.Knop_drie.UseVisualStyleBackColor = true;
+            this.Knop_drie.Click += new System.EventHandler(this.S);
+            // 
+            // Knop_plus
+            // 
+            resources.ApplyResources(this.Knop_plus, "Knop_plus");
+            this.Knop_plus.Name = "Knop_plus";
+            this.Knop_plus.UseVisualStyleBackColor = true;
+            // 
+            // Knop_min
+            // 
+            resources.ApplyResources(this.Knop_min, "Knop_min");
+            this.Knop_min.Name = "Knop_min";
+            this.Knop_min.UseVisualStyleBackColor = true;
+            this.Knop_min.Click += new System.EventHandler(this.Knop_min_Click);
+            // 
+            // Knop_zes
+            // 
+            resources.ApplyResources(this.Knop_zes, "Knop_zes");
+            this.Knop_zes.Name = "Knop_zes";
+            this.Knop_zes.UseVisualStyleBackColor = true;
+            this.Knop_zes.Click += new System.EventHandler(this.Knop_click);
+            // 
+            // Knop_vijf
+            // 
+            resources.ApplyResources(this.Knop_vijf, "Knop_vijf");
+            this.Knop_vijf.Name = "Knop_vijf";
+            this.Knop_vijf.UseVisualStyleBackColor = true;
+            this.Knop_vijf.Click += new System.EventHandler(this.Knop_click);
+            // 
+            // Knop_vier
+            // 
+            resources.ApplyResources(this.Knop_vier, "Knop_vier");
+            this.Knop_vier.Name = "Knop_vier";
+            this.Knop_vier.UseVisualStyleBackColor = true;
+            this.Knop_vier.Click += new System.EventHandler(this.Knop_click);
+            // 
+            // Knop_keer
+            // 
+            resources.ApplyResources(this.Knop_keer, "Knop_keer");
+            this.Knop_keer.Name = "Knop_keer";
+            this.Knop_keer.UseVisualStyleBackColor = true;
+            this.Knop_keer.Click += new System.EventHandler(this.Knop_keer_Click);
+            // 
+            // Knop_negen
+            // 
+            resources.ApplyResources(this.Knop_negen, "Knop_negen");
+            this.Knop_negen.Name = "Knop_negen";
+            this.Knop_negen.UseVisualStyleBackColor = true;
+            this.Knop_negen.Click += new System.EventHandler(this.Knop_click);
+            // 
+            // Knop_acht
+            // 
+            resources.ApplyResources(this.Knop_acht, "Knop_acht");
+            this.Knop_acht.Name = "Knop_acht";
+            this.Knop_acht.UseVisualStyleBackColor = true;
+            this.Knop_acht.Click += new System.EventHandler(this.Knop_click);
+            // 
+            // Knop_zeven
+            // 
+            resources.ApplyResources(this.Knop_zeven, "Knop_zeven");
+            this.Knop_zeven.Name = "Knop_zeven";
+            this.Knop_zeven.UseVisualStyleBackColor = true;
+            this.Knop_zeven.Click += new System.EventHandler(this.Knop_click);
+            // 
+            // Knop_delen
+            // 
+            resources.ApplyResources(this.Knop_delen, "Knop_delen");
+            this.Knop_delen.Name = "Knop_delen";
+            this.Knop_delen.UseVisualStyleBackColor = true;
+            this.Knop_delen.Click += new System.EventHandler(this.Knop_delen_Click);
+            // 
+            // Knop_backspace
+            // 
+            resources.ApplyResources(this.Knop_backspace, "Knop_backspace");
+            this.Knop_backspace.Name = "Knop_backspace";
+            this.Knop_backspace.UseVisualStyleBackColor = true;
+            this.Knop_backspace.Click += new System.EventHandler(this.Knop_backspace_Click);
+            // 
+            // Knop_C
+            // 
+            resources.ApplyResources(this.Knop_C, "Knop_C");
+            this.Knop_C.Name = "Knop_C";
+            this.Knop_C.UseVisualStyleBackColor = true;
+            this.Knop_C.Click += new System.EventHandler(this.Knop_C_Click);
+            // 
+            // Knop_CE
+            // 
+            resources.ApplyResources(this.Knop_CE, "Knop_CE");
+            this.Knop_CE.Name = "Knop_CE";
+            this.Knop_CE.UseVisualStyleBackColor = true;
+            this.Knop_CE.Click += new System.EventHandler(this.Knop_CE_Click);
+            // 
+            // Knop_percentage
+            // 
+            this.Knop_percentage.BackColor = System.Drawing.SystemColors.ControlLight;
+            resources.ApplyResources(this.Knop_percentage, "Knop_percentage");
+            this.Knop_percentage.Name = "Knop_percentage";
+            this.Knop_percentage.UseVisualStyleBackColor = false;
+            this.Knop_percentage.Click += new System.EventHandler(this.Knop_percentage_Click);
+            // 
+            // Knop_euro
+            // 
+            resources.ApplyResources(this.Knop_euro, "Knop_euro");
+            this.Knop_euro.Name = "Knop_euro";
+            this.Knop_euro.UseVisualStyleBackColor = true;
+            this.Knop_euro.Click += new System.EventHandler(this.Knop_euro_Click);
+            // 
+            // Textbox
+            // 
+            resources.ApplyResources(this.Textbox, "Textbox");
+            this.Textbox.Name = "Textbox";
+            this.Textbox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Rekenmachine
+            // 
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.Controls.Add(this.Textbox);
+            this.Controls.Add(this.Knop_percentage);
+            this.Controls.Add(this.Knop_euro);
+            this.Controls.Add(this.Knop_delen);
+            this.Controls.Add(this.Knop_backspace);
+            this.Controls.Add(this.Knop_C);
+            this.Controls.Add(this.Knop_CE);
+            this.Controls.Add(this.Knop_keer);
+            this.Controls.Add(this.Knop_negen);
+            this.Controls.Add(this.Knop_acht);
+            this.Controls.Add(this.Knop_zeven);
+            this.Controls.Add(this.Knop_min);
+            this.Controls.Add(this.Knop_zes);
+            this.Controls.Add(this.Knop_vijf);
+            this.Controls.Add(this.Knop_vier);
+            this.Controls.Add(this.Knop_plus);
+            this.Controls.Add(this.Knop_drie);
+            this.Controls.Add(this.Knop_2);
+            this.Controls.Add(this.Knop_1);
+            this.Controls.Add(this.Knop_is);
+            this.Controls.Add(this.Knop_haakje);
+            this.Controls.Add(this.Knop_nul);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Rekenmachine";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Knop_nul;
+        private System.Windows.Forms.Button Knop_haakje;
+        private System.Windows.Forms.Button Knop_is;
+        private System.Windows.Forms.Button Knop_1;
+        private System.Windows.Forms.Button Knop_2;
+        private System.Windows.Forms.Button Knop_drie;
+        private System.Windows.Forms.Button Knop_plus;
+        private System.Windows.Forms.Button Knop_min;
+        private System.Windows.Forms.Button Knop_zes;
+        private System.Windows.Forms.Button Knop_vijf;
+        private System.Windows.Forms.Button Knop_vier;
+        private System.Windows.Forms.Button Knop_keer;
+        private System.Windows.Forms.Button Knop_negen;
+        private System.Windows.Forms.Button Knop_acht;
+        private System.Windows.Forms.Button Knop_zeven;
+        private System.Windows.Forms.Button Knop_delen;
+        private System.Windows.Forms.Button Knop_backspace;
+        private System.Windows.Forms.Button Knop_C;
+        private System.Windows.Forms.Button Knop_CE;
+        private System.Windows.Forms.Button Knop_percentage;
+        private System.Windows.Forms.Button Knop_euro;
+        private System.Windows.Forms.TextBox Textbox;
     }
 }
 
--- Rekenmachine/Form1.cs
+++ Rekenmachine/Form1.cs
@@ -10,11 +10,96 @@ using System.Windows.Forms;
 
 namespace Rekenmachine
 {
-    public partial class Form1 : Form
+    public partial class Rekenmachine : Form
     {
-        public Form1()
+        public Rekenmachine()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button21_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button22_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Knop_euro_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Knop_percentage_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Knop_delen_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Knop_backspace_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Knop_C_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Knop_CE_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Knop_keer_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Knop_min_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Knop_click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void S(object sender, EventArgs e)
+        {
+
         }
     }
 }
