45449ef770927c9c4fe9f8db7a235bec4a8e3375
CDHelder <72396857+CDHelder@users.noreply.github.com>
2020-10-17 22:11:32 +0200

    17-10-2020

--- Rekenmachine/Form1.cs
+++ Rekenmachine/Form1.cs
@@ -12,7 +12,7 @@ namespace Rekenmachine
 {
     public partial class Rekenmachine : Form
     {
-        Double resultValue = 0;
+        Decimal resultValue = 0;
         String operationPerformed = "";
         bool isOperationPerformed = false;
 
@@ -29,17 +29,27 @@ namespace Rekenmachine
 
         private void Knop_click(object sender, EventArgs e)
         {
+            Button button = (Button)sender;
+
+            if (button.Text == ",")
+                    if (Textbox.Text == "0")
+                        Textbox.Text = "0" + button.Text;
+
+            if (Textbox.Text.Contains("%"))
+                return;
+
+            if (labelEuro.Text == "€") 
+            {
+                if (Textbox.Text.Contains(","))
+                    Textbox.Text = (decimal.Parse(Textbox.Text).ToString(#,##);
+                    //Hier moet komen dat bij € modus er niet maar max twee decimalen mogen worden ingevoerd
+
+            }
 
             if ((Textbox.Text == "0") || (isOperationPerformed))
                 Textbox.Clear();
 
-            /*if (labelEuro.Text == "€");
-            {
-                if (Textbox.Text.Contains(","))
-                    Textbox.Text = Textbox.Text.Substring(0, 2);
-                    //Hierzo moet nog de max twee decimalen in geval van € functie komen
-            
-            }
+            /*
             De functie werkt al doordat ie wordt afgerond
             Ik ben hier nu al een tijdje mee bezig en kan nergens op komen.
             Hij werkt gewoon goed alleen ziet het er appart uit
@@ -47,13 +57,18 @@ namespace Rekenmachine
             */
 
             isOperationPerformed = false;
-            Button button = (Button)sender;
             if (button.Text == ",")
             {
-                if (!Textbox.Text.Contains(","))
+                {
+                    if (Textbox.Text == "0")
+                    Textbox.Text = "0" + button.Text;
+
+                    else if(!Textbox.Text.Contains(","))
                     Textbox.Text += button.Text;
+                }   
+
             } else
-                Textbox.Text += button.Text;
+            Textbox.Text += button.Text;
         }
 
         private void operator_click(object sender, EventArgs e)
@@ -72,7 +87,7 @@ namespace Rekenmachine
             else
             {
                 operationPerformed = button.Text;
-                resultValue = Double.Parse(Textbox.Text);
+                resultValue = Decimal.Parse(Textbox.Text);
                 labelCurrentOperation.Text = resultValue + " " + operationPerformed;
                 isOperationPerformed = true;
                 Textbox.Text = "0";
@@ -95,34 +110,79 @@ namespace Rekenmachine
 
         private void Is_click(object sender, EventArgs e)
         {
-
+            /*
             if (Textbox.Text.Contains("%"))
             {
                 if (!Textbox.Text.Contains("%"))
-                    Textbox.Text = (1 / Double.Parse(Textbox.Text) * Double.Parse(Textbox.Text)).ToString();
+                    Textbox.Text += "%";
+            }
+            else
+                Textbox.Text += "%";
+
+            if (Textbox.Text.Contains("%"))
+            {
+                Textbox.Text.Replace("%", "");
+                Textbox.Text = ((1 / Double.Parse(Textbox.Text)) * resultValue).ToString();
             }
 
-            if (labelCurrentOperation.Text.Length != 0)
+            if (Textbox.Text.Contains("%"))
+                Textbox.Text.Replace("%", "");
+            */
+
+
+            if (Textbox.Text.Contains("%"))
+            {
+                Textbox.Text = Textbox.Text.Remove(Textbox.Text.Length - 1);
                 switch (operationPerformed)
                 {
                     case "+":
-                        Textbox.Text = (resultValue + Double.Parse(Textbox.Text)).ToString();
+                        decimal doubleTextboxText = Convert.ToDecimal(Textbox.Text);
+                        decimal TextboxTextPercentageValue = (1 / doubleTextboxText) * resultValue;
+                        Textbox.Text = (resultValue + TextboxTextPercentageValue).ToString();
                         break;
                     case "-":
-                        Textbox.Text = (resultValue - Double.Parse(Textbox.Text)).ToString();
+                        doubleTextboxText = Convert.ToDecimal(Textbox.Text);
+                        TextboxTextPercentageValue = (1 / doubleTextboxText) * resultValue;
+                        Textbox.Text = (resultValue - TextboxTextPercentageValue).ToString();
                         break;
                     case "*":
-                        Textbox.Text = (resultValue * Double.Parse(Textbox.Text)).ToString();
+                        doubleTextboxText = Convert.ToDecimal(Textbox.Text);
+                        TextboxTextPercentageValue = (1 / doubleTextboxText) * resultValue;
+                        Textbox.Text = (resultValue * TextboxTextPercentageValue).ToString();
                         break;
                     case "/":
-                        Textbox.Text = (resultValue / Double.Parse(Textbox.Text)).ToString();
+                        doubleTextboxText = Convert.ToDecimal(Textbox.Text);
+                        TextboxTextPercentageValue = (1 / doubleTextboxText) * resultValue;
+                        Textbox.Text = (resultValue / TextboxTextPercentageValue).ToString();
+                        break;
+                    default:
+                        break;
+                }
+
+
+            }
+            else if (labelCurrentOperation.Text.Length != 0)
+                switch (operationPerformed)
+                {
+                    case "+":
+                        Textbox.Text = (resultValue + Decimal.Parse(Textbox.Text)).ToString();
+                        break;
+                    case "-":
+                        Textbox.Text = (resultValue - Decimal.Parse(Textbox.Text)).ToString();
+                        break;
+                    case "*":
+                        Textbox.Text = (resultValue * Decimal.Parse(Textbox.Text)).ToString();
+                        break;
+                    case "/":
+                        Textbox.Text = (resultValue / Decimal.Parse(Textbox.Text)).ToString();
                         break;
 
                 }
-            if (labelEuro.Text == "€") ;
-            Textbox.Text = (Math.Round(Double.Parse(Textbox.Text), 2).ToString());
 
-            resultValue = Double.Parse(Textbox.Text);
+            if (labelEuro.Text == "€") 
+            Textbox.Text = (Math.Round(Decimal.Parse(Textbox.Text), 2).ToString());
+
+            resultValue = Decimal.Parse(Textbox.Text);
             labelCurrentOperation.Text = "";
         }
 
@@ -147,13 +207,15 @@ namespace Rekenmachine
 
         private void Percentage_knop(object sender, EventArgs e)
         {
-            if (labelCurrentOperation.Text.Contains("+") || (labelCurrentOperation.Text.Contains("*") || (labelCurrentOperation.Text.Contains("-") || labelCurrentOperation.Text.Contains("/")
+            if (labelCurrentOperation.Text.Contains("+") || (labelCurrentOperation.Text.Contains("*") || (labelCurrentOperation.Text.Contains("-") || labelCurrentOperation.Text.Contains("/"))))
+            {
                 if (Textbox.Text.Contains("%"))
                 {
                     if (!Textbox.Text.Contains("%"))
                         Textbox.Text += "%";
                 } else
                     Textbox.Text += "%";
+            }
         }
     }
 }
